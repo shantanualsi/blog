f4406ebd0b998a6ce2aec5b18d68a046d9515e2e
shantanualshi <shantanuals@gmail.com>
2021-07-24 16:29:20 +0800

    Re-enable google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,12 +54,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
